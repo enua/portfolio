b1f55b8b26ed08aea55b4e8d6f174c34289b2a36
Enrique Nuñez <enrique.nunyez@gmail.com>
2021-04-26 18:37:59 +0200

    (PF-02): :construction: main component, card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatTableModule} from '@angular/material/table';
 import {MatSortModule} from '@angular/material/sort';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
@@ -46,6 +47,7 @@ import {MatPaginatorModule} from '@angular/material/paginator';
     HeaderComponent,
     FooterComponent,
     MainComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from '../../models/card';
 
 @Component({
   selector: 'app-main',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.sass']
 })
 export class MainComponent implements OnInit {
+  cards: Card[] = [{
+    width: '300px',
+    title: 'Angular Rover',
+    image: '/assets/images/angular_rover_01.PNG',
+    content: 'The marthian rover was sended to save the planet',
+    link: '/angular-rover/',
+    social: '/',
+  }]
 
   constructor() { }
 
